ea0ee91b203f2f53d8bcf219fcebf76d0111a4d2
oktaryan-h <oktaryan.h@gmail.com>
2019-07-19 15:29:37 +0700

    Theme R4

--- functions.php
+++ functions.php
@@ -14,14 +14,19 @@ function wp_hook_head() {
       <script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script>
   <![endif]-->
 
-  <?php wp_enqueue_script("jquery");
+  <?php
 }
+
+// function wp_hook_enqueue_scripts() {
+// 	wp_enqueue_script("jquery");
+// }
 
 function wp_bootstrap_jquery() {
 	// Register the script like this for a theme:
 	wp_register_script( 'custom-script', get_template_directory_uri() . '/bootstrap/js/bootstrap.js', array( 'jquery' ) );
 	// For either a plugin or a theme, you can then enqueue the script:
 	wp_enqueue_script( 'custom-script' );
+	wp_enqueue_script('jquery');
 }
 
 function register_main_sidebar() {
