5ef6f56fa3381ba55d01d68a2e0356c17191f31e
oktaryan-h <oktaryan.h@gmail.com>
2019-07-22 13:40:39 +0700

    Theme R17

--- comments.php
+++ comments.php
@@ -1,57 +1,83 @@
-<div id="comments" class="comments-area">
+<?php
+/**
+ * The template for displaying comments
+ *
+ * The area of the page that contains both current comments
+ * and the comment form.
+ *
+ * @package WordPress
+ * @subpackage Twenty_Sixteen
+ * @since Twenty Sixteen 1.0
+ */
 
-	<?php // You can start editing here -- including this comment! ?>
+/*
+ * If the current post is protected by a password and
+ * the visitor has not yet entered the password we will
+ * return early without loading the comments.
+ */
+if ( post_password_required() ) {
+	return;
+}
+?>
+
+<div id="comments" class="comments-area">
 
 	<?php if ( have_comments() ) : ?>
 		<h2 class="comments-title">
 			<?php
-			if ( 1 === get_comments_number() ) {
-				printf(
-					/* translators: %s: The post title. */
-					__( 'One thought on &ldquo;%s&rdquo;', 'twentytwelve' ),
-					'<span>' . get_the_title() . '</span>'
-				);
+			$comments_number = get_comments_number();
+			if ( '1' === $comments_number ) {
+				/* translators: %s: post title */
+				printf( _x( 'One thought on &ldquo;%s&rdquo;', 'comments title', 'twentysixteen' ), get_the_title() );
 			} else {
 				printf(
-					/* translators: %1$s: The number of comments. %2$s: The post title. */
-					_n( '%1$s thought on &ldquo;%2$s&rdquo;', '%1$s thoughts on &ldquo;%2$s&rdquo;', get_comments_number(), 'twentytwelve' ),
-					number_format_i18n( get_comments_number() ),
-					'<span>' . get_the_title() . '</span>'
+					/* translators: 1: number of comments, 2: post title */
+					_nx(
+						'%1$s thought on &ldquo;%2$s&rdquo;',
+						'%1$s thoughts on &ldquo;%2$s&rdquo;',
+						$comments_number,
+						'comments title',
+						'twentysixteen'
+					),
+					number_format_i18n( $comments_number ),
+					get_the_title()
 				);
 			}
 			?>
 		</h2>
 
-		<ol class="commentlist">
+		<?php the_comments_navigation(); ?>
+
+		<ol class="comment-list">
 			<?php
 			wp_list_comments(
 				array(
-					'callback' => 'twentytwelve_comment',
-					'style'    => 'ol',
+					'style'       => 'ol',
+					'short_ping'  => true,
+					'avatar_size' => 42,
 				)
 			);
 			?>
-		</ol><!-- .commentlist -->
+		</ol><!-- .comment-list -->
 
-		<?php if ( get_comment_pages_count() > 1 && get_option( 'page_comments' ) ) : // are there comments to navigate through ?>
-		<nav id="comment-nav-below" class="navigation" role="navigation">
-			<h1 class="assistive-text section-heading"><?php _e( 'Comment navigation', 'twentytwelve' ); ?></h1>
-			<div class="nav-previous"><?php previous_comments_link( __( '&larr; Older Comments', 'twentytwelve' ) ); ?></div>
-			<div class="nav-next"><?php next_comments_link( __( 'Newer Comments &rarr;', 'twentytwelve' ) ); ?></div>
-		</nav>
-		<?php endif; // check for comment navigation ?>
+		<?php the_comments_navigation(); ?>
 
-		<?php
-		/* If there are no comments and comments are closed, let's leave a note.
-		 * But we only want the note on posts and pages that had comments in the first place.
-		 */
-		if ( ! comments_open() && get_comments_number() ) :
-			?>
-		<p class="nocomments"><?php _e( 'Comments are closed.', 'twentytwelve' ); ?></p>
-		<?php endif; ?>
+	<?php endif; // Check for have_comments(). ?>
 
-	<?php endif; // have_comments() ?>
+	<?php
+		// If comments are closed and there are comments, let's leave a little note, shall we?
+	if ( ! comments_open() && get_comments_number() && post_type_supports( get_post_type(), 'comments' ) ) :
+		?>
+	<p class="no-comments"><?php _e( 'Comments are closed.', 'twentysixteen' ); ?></p>
+<?php endif; ?>
 
-	<?php comment_form(); ?>
+<?php
+comment_form(
+	array(
+		'title_reply_before' => '<h2 id="reply-title" class="comment-reply-title">',
+		'title_reply_after'  => '</h2>',
+	)
+);
+?>
 
-</div><!-- #comments .comments-area --
+</div><!-- .comments-area -->
